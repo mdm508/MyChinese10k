11a148df924d36f114e223cc82b573c63953871f
Matthew McLaughlin <matthedm@uci.edu>
2023-07-11 10:00:06 -0700

    Initial Commit

---
+++ ChineseWordOfTheDay/ChineseWordOfTheDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseWordOfTheDayApp.swift
+//  ChineseWordOfTheDay
+//
+//  Created by m on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ChineseWordOfTheDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
